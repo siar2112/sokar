489e9a50a23c550a9f4ecb2299a54d5cd0d7c310
siar2112 <87784451+siar2112@users.noreply.github.com>
2023-07-28 18:02:49 -0400

    despair 3

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const path = require('path');
 
 
 // Serve any static files
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Handle React routing, return all requests to React app
 app.get('*', function(req, res) {
