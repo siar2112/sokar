dfe74c3c2e07db1b387404f39636c262bbb49c95
siar2112 <87784451+siar2112@users.noreply.github.com>
2023-07-28 16:14:39 -0400

    updated start script

--- server/server.js
+++ server/server.js
@@ -10,15 +10,15 @@ const session = require('express-session');
 
 const path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
 
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
+// Serve any static files
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 
 
 
